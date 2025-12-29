cb0c43af2822b8ca3cb2c222d959b58e5b4383bd
Wikene77 <hw@systemair.no>
2025-12-29 19:44:51 +0100

    Update const.py

--- custom_components/tommekalender_sandnes/const.py
+++ custom_components/tommekalender_sandnes/const.py
@@ -3,6 +3,8 @@ DOMAIN = "tommekalender_sandnes"
 DEFAULT_NAME = "Tømmekalender"
 DEFAULT_SCAN_INTERVAL_HOURS = 12
 
+# LEFT side must match text found in hentavfall.no HTML (img alt/title etc.)
+# RIGHT side is used as slug in entity names.
 WASTE_TYPES = {
     "Restavfall": "restavfall",
     "Matavfall": "matavfall",
