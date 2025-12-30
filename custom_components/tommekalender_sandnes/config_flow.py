73b552d85ea0af3deccdaa6a33e5852c30de3868
Wikene77 <hw@systemair.no>
2025-12-30 13:14:58 +0100

    Update config_flow.py

--- custom_components/tommekalender_sandnes/config_flow.py
+++ custom_components/tommekalender_sandnes/config_flow.py
@@ -13,14 +13,19 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             await self.async_set_unique_id(user_input["url"])
             self._abort_if_unique_id_configured()
+
+            title = (user_input.get("name") or "Tømmekalender").strip()
+
             return self.async_create_entry(
-                title="Tømmekalender",
+                title=title,
                 data=user_input,
             )
 
-        schema = vol.Schema({
-            vol.Required("url"): str,
-            vol.Optional("name", default="Tømmekalender"): str,
-        })
+        schema = vol.Schema(
+            {
+                vol.Required("url"): str,
+                vol.Optional("name", default="Tømmekalender Sandnes Kommune"): str,
+            }
+        )
 
         return self.async_show_form(step_id="user", data_schema=schema)
